209de9911caa16d3956879d8690d2894747d248e
Yaroslav <hodosov.y@mail.ru>
2022-01-30 21:35:56 +0300

    add images

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { WelcomeStep } from '../components/steps/WelcomeStep'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container} />
+    <div className={styles.container} >
+      <WelcomeStep />
+    </div>
   )
 }
