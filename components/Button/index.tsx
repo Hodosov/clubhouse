af47ad170ac37a22d937dbed966d5e9e813a87c9
Yaroslav <hodosov.y@mail.ru>
2022-02-01 22:27:16 +0300

    fit js => tsx

---
+++ components/Button/index.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import React, { FC } from "react";
+
+import styles from "./Button.module.scss";
+
+interface ButtonProps {
+    disabled?: boolean;
+    color?: string;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    className?: string;
+}
+
+const colors = {
+    green: styles.buttonGreen,
+    gray: styles.buttonGray,
+    blue: styles.buttonBlue,
+};
+
+export const Button: FC<ButtonProps> = ({
+    children,
+    disabled,
+    color,
+    onClick,
+    className,
+}) => {
+    return (
+        <button
+            onClick={onClick}
+            type="button"
+            className={clsx(className, styles.button, colors[color])}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
---
+++ components/steps/ChooseAvatarStep/index.tsx
@@ -0,0 +1,51 @@
+import React, { FC, ReactFragment, useEffect, useState } from "react";
+import clsx from "clsx";
+import { WhiteBlock } from "../../WhiteBlock";
+import { Button } from "../../Button";
+import { StepInfo } from "../../StepInfo";
+import { Avatar } from "../../Avatar";
+
+import styles from "./ChooseAvatarStep.module.scss";
+import { useRef } from "react";
+
+export const ChooseAvatarStep: FC = () => {
+    const [avatarUrl, setAvatarUrl] = useState<string>("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleChangeImage = (event: Event): void => {
+        const file = (event.target as HTMLInputElement).files[0];
+        const imgUrl = URL.createObjectURL(file);
+        setAvatarUrl(imgUrl);
+    };
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.addEventListener("change", handleChangeImage);
+        }
+    }, []);
+
+    return (
+        <div className={styles.block}>
+            <StepInfo
+                icon="/static/celebration.png"
+                title={`Okay!`}
+                description="How’s this photo?"
+            />
+            <WhiteBlock className={clsx("m-auto mt-40", styles.whiteBlock)}>
+                <div className={styles.avatar}>
+                    <Avatar width="120px" height="120px" src={avatarUrl} />
+                </div>
+                <div className="mb-30">
+                    <label htmlFor="image" className="link cup">
+                        Choose a different photo
+                    </label>
+                </div>
+                <input id="image" ref={inputRef} type="file" hidden />
+                <Button>
+                    Next
+                    <img className="d-ib ml-10" src="/static/arrow.svg" />
+                </Button>
+            </WhiteBlock>
+        </div>
+    );
+};
