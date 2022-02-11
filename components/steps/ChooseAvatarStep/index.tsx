dcde9d9d4842b0c2d6a23e492eaaa521ba6160d5
Yaroslav <hodosov.y@mail.ru>
2022-02-11 21:51:11 +0300

    upload avatar

--- components/steps/ChooseAvatarStep/index.tsx
+++ components/steps/ChooseAvatarStep/index.tsx
@@ -1,9 +1,9 @@
 import React, {
-    FC,
-    ReactFragment,
-    useContext,
-    useEffect,
-    useState,
+  FC,
+  ReactFragment,
+  useContext,
+  useEffect,
+  useState,
 } from "react";
 import clsx from "clsx";
 import { WhiteBlock } from "../../WhiteBlock";
@@ -14,48 +14,63 @@ import { Avatar } from "@components/Avatar";
 import styles from "./ChooseAvatarStep.module.scss";
 import { useRef } from "react";
 import { MainContext } from "pages";
+import axios from "@core/axios";
+
+const uploadFile = async (file: File) => {
+  const formData = new FormData();
+  formData.append("photo", file);
+  const { data } = await axios.post("/upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return data;
+};
 
 export const ChooseAvatarStep: FC = () => {
-    const { onNextStep } = useContext(MainContext);
-    const [avatarUrl, setAvatarUrl] = useState<string>("");
-    const inputRef = useRef<HTMLInputElement>(null);
+  const { onNextStep } = useContext(MainContext);
+  const [avatarUrl, setAvatarUrl] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleChangeImage = (event: Event): void => {
-        const file = (event.target as HTMLInputElement).files[0];
-        if (file) {
-            const imgUrl = URL.createObjectURL(file);
-            setAvatarUrl(imgUrl);
-        }
-    };
+  const handleChangeImage = async (event: Event) => {
+    const file = (event.target as HTMLInputElement).files[0];
+    if (file) {
+      const imgUrl = URL.createObjectURL(file);
+      setAvatarUrl(imgUrl);
+      const data = await uploadFile(file);
+      console.log(data);
+    }
+  };
 
-    useEffect(() => {
-        if (inputRef.current) {
-            inputRef.current.addEventListener("change", handleChangeImage);
-        }
-    }, []);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.addEventListener("change", handleChangeImage);
+    }
+  }, []);
 
-    return (
-        <div className={styles.block}>
-            <StepInfo
-                icon="/static/celebration.png"
-                title={`Okay!`}
-                description="How’s this photo?"
-            />
-            <WhiteBlock className={clsx("m-auto mt-40", styles.whiteBlock)}>
-                <div className={styles.avatar}>
-                    <Avatar width="120px" height="120px" src={avatarUrl} />
-                </div>
-                <div className="mb-30">
-                    <label htmlFor="image" className="link cup">
-                        Choose a different photo
-                    </label>
-                </div>
-                <input id="image" ref={inputRef} type="file" hidden />
-                <Button onClick={onNextStep}>
-                    Next
-                    <img className="d-ib ml-10" src="/static/arrow.svg" />
-                </Button>
-            </WhiteBlock>
+  return (
+    <div className={styles.block}>
+      <StepInfo
+        icon="/static/celebration.png"
+        title={`Okay!`}
+        description="How’s this photo?"
+      />
+      <WhiteBlock className={clsx("m-auto mt-40", styles.whiteBlock)}>
+        <div className={styles.avatar}>
+          <Avatar width="120px" height="120px" src={avatarUrl} />
         </div>
-    );
+        <div className="mb-30">
+          <label htmlFor="image" className="link cup">
+            Choose a different photo
+          </label>
+        </div>
+        <input id="image" ref={inputRef} type="file" hidden />
+        <Button onClick={onNextStep}>
+          Next
+          <img className="d-ib ml-10" src="/static/arrow.svg" />
+        </Button>
+      </WhiteBlock>
+    </div>
+  );
 };
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,34 @@
 import express from "express";
 import { passport } from "./core/passport";
+import multer from "multer";
+import { nanoid } from "nanoid";
+import cors from "cors";
 
 import "./core/db";
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+const uploader = multer({
+  storage: multer.diskStorage({
+    destination: function (_, __, cb) {
+      cb(null, "public/avatars");
+    },
+    filename: function (_, file, cb) {
+      cb(
+        null,
+        file.fieldname + "-" + nanoid(6) + "." + file.mimetype.split("/").pop()
+      );
+    },
+  }),
+});
+
+app.use(cors());
+
+app.post("/upload", uploader.single("photo"), (req, res) => {
+  res.json(req.file);
+});
 
 app.get("/auth/github", passport.authenticate("github"));
 
