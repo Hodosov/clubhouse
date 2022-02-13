f983aa8a33ad338b1ec82ab200d5e90d66761350
Yaroslav <hodosov.y@mail.ru>
2022-02-13 20:01:08 +0300

    refactoring

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,15 @@
 import express from "express";
 import fs from "fs";
 import { passport } from "./core/passport";
-import multer from "multer";
-import { nanoid } from "nanoid";
 import sharp from "sharp";
 import cors from "cors";
 
 import "./core/db";
-import { UserData } from "pages";
 import AuthController from "./controllers/AuthController";
-
-declare global {
-  namespace Express {
-    interface User extends UserData {}
-  }
-}
+import { uploader } from "./core/uploader";
 
 const app = express();
-
 const PORT = process.env.PORT;
-
-const uploader = multer({
-  storage: multer.diskStorage({
-    destination: function (_, __, cb) {
-      cb(null, "public/avatars");
-    },
-    filename: function (_, file, cb) {
-      cb(
-        null,
-        file.fieldname + "-" + nanoid(6) + "." + file.mimetype.split("/").pop()
-      );
-    },
-  }),
-});
 
 app.use(cors());
 app.use(express.json());
